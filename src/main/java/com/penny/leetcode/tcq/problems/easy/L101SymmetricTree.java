13732f35f4c2105cbce89cd40aa4fc1a2683307c
0-Vector <Answer1331@163.com>
2019-11-23 17:24:58 +0800

    problem-101 【优化】

--- src/main/java/com/penny/leetcode/tcq/problems/easy/L101SymmetricTree.java
+++ src/main/java/com/penny/leetcode/tcq/problems/easy/L101SymmetricTree.java
@@ -34,6 +34,24 @@ public class L101SymmetricTree {
         if (root == null) {
             return true;
         }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null ^ right == null) {
+            return false;
+        } else if (left == right) {
+            return true;
+        } else if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    public boolean isSymmetricV2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
 
         Queue<TreeNode> queue = new LinkedList<>();
         if (root.left != null && root.right != null && root.left.val == root.right.val) {
