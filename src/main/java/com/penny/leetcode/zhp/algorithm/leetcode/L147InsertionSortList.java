7bb3bfe64063baf90b7519193714cf8288252d7f
zhangpeng1990618 <17071400190@163.com>
2019-12-13 00:42:18 +0800

    L1042 L147

---
+++ src/main/java/com/penny/leetcode/zhp/algorithm/leetcode/L147InsertionSortList.java
@@ -0,0 +1,20 @@
+package com.penny.leetcode.zhp.algorithm.leetcode;
+
+/**对链表进行插入排序
+ * @author zhangpeng110
+ * @create 2019/12/13 0013
+ * @desc
+ * https://leetcode-cn.com/problems/insertion-sort-list/
+ */
+public class L147InsertionSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+      return null;
+    }
+
+    public class ListNode {
+             int val;
+           ListNode next;
+            ListNode(int x) { val = x; }
+        }
+}
