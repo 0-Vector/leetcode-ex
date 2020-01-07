2af5b428bae59c08c70fb74a6cd5d896c94ac0b6
zhangpeng1990618 <17071400190@163.com>
2020-01-07 23:51:54 +0800

    L1042 L147

--- src/main/java/com/penny/leetcode/zhp/algorithm/leetcode/L11ContainerWithMostWater.java
+++ src/main/java/com/penny/leetcode/zhp/algorithm/leetcode/L11ContainerWithMostWater.java
@@ -15,8 +15,15 @@ package com.penny.leetcode.zhp.algorithm.leetcode;
 public class L11ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-
-      return  0;
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
     }
 
 }
